740d19a0f0997e071ab7959cc910a0f43e6c9b35
Banja David Lotigo <innocdavid@gmail.com>
2024-03-04 10:27:40 +0300

    refactor code to use people you may know and trending topic components

--- connect-gui/src/views/SearchView.vue
+++ connect-gui/src/views/SearchView.vue
@@ -103,84 +103,21 @@
         </div>
 
         <div class="main-right col-span-1 space-y-4">
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-xl">People you may now</h3>
-
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full" />
-                            <p class="text-xs"><strong>Code With Innoc</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full" />
-                            <p class="text-xs"><strong>Code With Innoc</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full" />
-                            <p class="text-xs"><strong>Code With Innoc</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full" />
-                            <p class="text-xs"><strong>Code With Innoc</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-
-                    </div>
-                </div>
-
-            </div>
-            <div class="p-4 bg-white border border-gray-200 rounded-l">
-                <h3 class="mb-6 text-xl">Trending</h3>
-
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#nature</strong><br>
-                            <span class="text-gray=500">200 posts</span>
-                        </p>
-                         <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#nature</strong><br>
-                            <span class="text-gray=500">200 posts</span>
-                        </p>
-                         <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#nature</strong><br>
-                            <span class="text-gray=500">200 posts</span>
-                        </p>
-                         <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#nature</strong><br>
-                            <span class="text-gray=500">200 posts</span>
-                        </p>
-                         <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                </div>
-            </div>
+            <PeopleYouMayKnow />
+            <TrendingTopics />
         </div>
     </div>
     
 </template>
+<script>
+    import PeopleYouMayKnow from '@/components/PeopleYouMayKnow.vue';
+    import TrendingTopics from '@/components/TrendingTopics.vue'; 
+
+    export default {
+        name: 'SearchView',
+        components: {
+            PeopleYouMayKnow,
+            TrendingTopics,
+        },
+    }
+</script>

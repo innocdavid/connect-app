3dea25d4c8335e5e293ea324295087cb8bdaac04
Banja David Lotigo <innocdavid@gmail.com>
2024-05-16 14:56:34 +0300

    [middleware] fix cors middleware

--- server/server/settings.py
+++ server/server/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.Cor'
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

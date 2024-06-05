e3fc7cd98830824e009c2b0f2fae88557c43161d
Banja David Lotigo <innocdavid@gmail.com>
2024-06-05 11:15:12 +0300

    [client] update app component

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,14 +4,17 @@
     <main class="px-8 py-6 bg-gray-100">
       <RouterView />
     </main> 
-
+    <Toast />
 </template>
 <script>
-    import NavBar from './components/NavBar.vue';
+    import NavBar from '@/components/NavBar.vue';
+    import Toast from '@/components/Toast.vue'
+
     export default {
         name: 'App',
         components: {
             NavBar,
+            Toast,
         },
     }
 </script>
